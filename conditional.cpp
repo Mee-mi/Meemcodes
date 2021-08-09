1efe38233074210059ba15a9b2db202c9e77eff9
Maryam Irshad <88659675+Mee-mi@users.noreply.github.com>
2021-08-09 23:40:15 +0500

    updated conditional

--- conditional.cpp
+++ conditional.cpp
@@ -3,55 +3,55 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-
+using namespace std;
 
 
 int main() {
     int n;
-    std::cin >> n;
+   cin >> n;
    
  if (n==1)
  
  {
-  std::cout<< "one" ;
+ cout<< "one" ;
   }
      else if (n==2)
      {
-          std::cout<< "two" ;
+          cout<< "two" ;
     }
     else if (n==3)
      {
-          std::cout<< "three" ;
+          cout<< "three" ;
      }
      else if (n==4)
      {
-          std::cout<< "four" ;
+          cout<< "four" ;
      }
      else if (n==5)
      {
-          std::cout<< "five" ;
+           cout<< "five" ;
      }
      else if (n==6)
      {
-          std::cout<< "six" ;
+         cout<< "six" ;
      }
      else if (n==7)
      {
-          std::cout<< "seven" ;
+         cout<< "seven" ;
   
      }
      else if (n==8)
      {
-          std::cout<< "eight" ;
+        cout<< "eight" ;
      }
      else if (n==9)
      {
-          std::cout<< "nine" ;
+          cout<< "nine" ;
      }
      
 else
 {
-    std::cout<< "greater than 9" ;
+    cout<< "Greater than 9" ;
     
 }
 return 0;
